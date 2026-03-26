921b4a5eb7cba35fe1f362545006a1aac3e3987f
Jordon <jordon.mcleod@rdpolytech.ca>
2026-03-26 09:20:47 -0600

    msg

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //This is our app! Woo!
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+//The slash is the root of a domain
+
+//Route Handler
+app.get('/', (req, res) => [
+    res.send("Hello, World!")
+]);
+//controllers tend to give views
+//API end points serve data
+
+//Server Setup
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
