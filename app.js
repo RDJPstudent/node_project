61631536569b069da95237640ee25b427f10561a
Jordon <jordon.mcleod@rdpolytech.ca>
2026-04-01 09:15:53 -0600

    gg3

--- app.js
+++ app.js
@@ -1,57 +1,107 @@
-//This is our app! Woo!
-const express = require('express');
+// //This is our app! Woo!
+// const express = require('express');
+// const app = express();
+// const ejs = require('ejs');
+// const mongoose = require('mongoose');
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const uri = process.env.MONGO_URI;
+// const port = process.env.PORT || 8000;
+
+// //added on 2nd day
+// const userRoutes = require(`./routes/userRoutes`);
+
+// //View engine
+// app.set('view engine', 'ejs');
+// app.set( 'views', './views');
+// // The "./" is for relative path.
+
+
+
+
+// //added on 2nd day, GLOBAL middleware
+// app.use(express.static('public'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true}));
+// app.use(userRoutes);
+// // need access to static file, but before it!
+
+
+// //The slash is the root of a domain
+
+// //Route Handler
+// app.get('/', (req, res) => [
+//     res.send("Hello, World!")
+// ]);
+// //controllers tend to give views
+// //API end points serve data
+
+// //Connect to MongoDB
+// // mongoose.connect(uri).then(() => {
+// //     console.log('Connected to MongoDB');
+// //     //server setp
+// //     app.listen(port, ()=> {
+// //         console.log(`Server is running on port ${port}`);
+// //     });
+// //     catch.((err) => {
+// //         console.log(`Server is running on port ${port}`);
+// //     })
+    
+// // });
+
+// // }).catch(err) => {
+// //     console.log(`Error connecting o mongoDB: {err}`);
+// // }
+
+// //connect
+// mongoose
+//   .connect(uri)
+//   .then(async () => {
+//     console.log("Connected to MongoDB");
+
+//     //server setup
+//     app.listen(port, () => {
+//       console.log(`Server is running on port ${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(`Error connecting to MongoDB: ${err}`);
+//   });
+
+// //Server Setup
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const mongoose = require('mongoose');
+
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const uri = process.env.MONGO_URI;
 const port = process.env.PORT || 8000;
 
-//added on 2nd day
-const userRoutes = require(`./routes/userRoutes`);
+const userRoutes = require("./routes/userRoutes");
 
-//View engine
-app.set('view engine', 'ejs');
-app.set( 'views', './views');
-// The "./" is for relative path.
+//view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-
-
-
-//added on 2nd day, GLOBAL middleware
-app.use(express.static('public'));
+//global middleware
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(userRoutes);
-// need access to static file, but before it!
+
+//route handler
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 
-//The slash is the root of a domain
-
-//Route Handler
-app.get('/', (req, res) => [
-    res.send("Hello, World!")
-]);
-//controllers tend to give views
-//API end points serve data
-
-//Connect to MongoDB
-// mongoose.connect(uri).then(() => {
-//     console.log('Connected to MongoDB');
-//     //server setp
-//     app.listen(port, ()=> {
-//         console.log(`Server is running on port ${port}`);
-//     });
-//     catch.((err) => {
-//         console.log(`Server is running on port ${port}`);
-//     })
-    
-// });
-
-// }).catch(err) => {
-//     console.log(`Error connecting o mongoDB: {err}`);
-// }
 
 //connect
 mongoose
@@ -67,9 +117,3 @@ mongoose
   .catch((err) => {
     console.log(`Error connecting to MongoDB: ${err}`);
   });
-
-//Server Setup
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
